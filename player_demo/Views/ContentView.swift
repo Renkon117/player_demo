04ce18520cfcae58a62bc1997f6d234e7b722df6
Rendai Okada <renkon.daikon1020@gmail.com>
2024-05-31 14:16:29 +0900

    tutorial Building lists and navigation completed

---
+++ player_demo/Views/ContentView.swift
@@ -0,0 +1,19 @@
+//
+//  ContentView.swift
+//  player_demo
+//
+//  Created by Rendai Okada on 2024/05/30.
+//
+
+import SwiftUI
+import SwiftData
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+    }
+}
+
+#Preview {
+    ContentView()
+}
